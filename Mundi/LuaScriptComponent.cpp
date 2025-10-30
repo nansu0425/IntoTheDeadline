515ab0384622bc8e2bd7617147eb720067668975
Soyeon Joo <sildredel@naver.com>
2025-10-31 08:00:30 +0900

    feat: lua 연결

---
+++ Mundi/LuaScriptComponent.cpp
@@ -0,0 +1,78 @@
+﻿#include "pch.h"
+#include "LuaScriptComponent.h"
+
+#include <state.hpp>
+
+IMPLEMENT_CLASS(ULuaScriptComponent)
+
+BEGIN_PROPERTIES(ULuaScriptComponent)
+	MARK_AS_COMPONENT("Lua 스크립트 컴포넌트", "Lua 스크립트 컴포넌트입니다.")
+END_PROPERTIES()
+
+ULuaScriptComponent::ULuaScriptComponent()
+{
+}
+
+ULuaScriptComponent::~ULuaScriptComponent()
+{
+}
+
+void ULuaScriptComponent::BeginPlay()
+{
+	sol::state lua;
+	lua.open_libraries(sol::lib::base);
+
+	lua.new_usertype<FVector>("Vector",
+		sol::constructors<FVector(), FVector(float, float, float)>(),
+		"x", &FVector::X,
+		"y", &FVector::Y,
+		"z", &FVector::Z,
+		sol::meta_function::addition, [](const FVector& a, const FVector& b) { return FVector(a.X + b.X, a.Y + b.Y, a.Z + b.Z); },
+		sol::meta_function::multiplication, [](const FVector& v, float f) { return v * f; }
+	);
+
+	lua.new_usertype<GameObject>("GameObject",
+		"UUID", &GameObject::UUID,
+		"Location", &GameObject::Location,
+		"Velocity", &GameObject::Velocity,
+		"PrintLocation", &GameObject::PrintLocation
+	);
+
+	GameObject obj;
+	obj.Location = FVector(0, 0, 0);
+	obj.Velocity = FVector(10, 0, 0);
+
+	GameObject obj2;
+	obj2.Location = FVector(0, 0, 0);
+	obj2.Velocity = FVector(9, 9, 9);
+
+	lua["obj"] = &obj;
+
+	lua.script_file("Data/Scripts/template.lua");
+
+	//lua.script(R"(
+	//  function Tick(dt)
+	//      obj.Location = obj.Location + obj.Velocity * dt
+	//  end
+	//)");
+
+	lua["BeginPlay"]();
+
+	// 테스트 루프
+	for (int i = 0; i < 5; ++i) {
+		lua["Tick"](0.1f); // delta time = 0.1
+	}
+
+	lua["OnOverlap"](obj2);
+
+	lua["EndPlay"]();
+}
+
+void ULuaScriptComponent::TickComponent(float DeltaTime)
+{
+
+}
+
+void ULuaScriptComponent::EndPlay(EEndPlayReason Reason)
+{
+}
---
+++ Mundi/Source/Runtime/Engine/Components/LuaScriptComponent.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "ActorComponent.h"
+#include "Vector.h"
+
+class GameObject
+{
+public:
+	uint32  UUID;
+	FVector Location;
+	FVector Velocity;
+
+	void PrintLocation()
+	{
+		UE_LOG("Location %f %f %f\n", Location.X, Location.Y, Location.Z);
+	}
+};
+
+class USceneComponent;
+
+class ULuaScriptComponent : public UActorComponent
+{
+public:
+    DECLARE_CLASS(ULuaScriptComponent, UActorComponent)
+	GENERATED_REFLECTION_BODY()
+    DECLARE_DUPLICATE(ULuaScriptComponent)
+
+    ULuaScriptComponent();
+	~ULuaScriptComponent() override;
+protected:
+   
+
+public:
+	void BeginPlay() override;
+	void TickComponent(float DeltaTime) override;       // 매 프레임
+	void EndPlay(EEndPlayReason Reason) override;       // 파괴/월드 제거 시
+
+protected:
+    // 이 컴포넌트가 실행할 .lua 스크립트 파일의 경로 (에디터에서 설정)
+    FString ScriptFilePath;
+};
