ea857ae687da7d77f15e28d3b0b0241aed41ae48
Soyeon Joo <sildredel@naver.com>
2025-11-06 03:38:05 +0900

    feat: game 빌드일 때 log 없앰

--- Mundi/Source/Slate/GlobalConsole.cpp
+++ Mundi/Source/Slate/GlobalConsole.cpp
@@ -35,14 +35,17 @@ UConsoleWidget* UGlobalConsole::GetConsoleWidget()
 
 void UGlobalConsole::Log(const char* fmt, ...)
 {
+#ifdef _EDITOR
     va_list args;
     va_start(args, fmt);
     LogV(fmt, args);
     va_end(args);
+#endif
 }
 
 void UGlobalConsole::LogV(const char* fmt, va_list args)
 {
+#ifdef _EDITOR
     if (ConsoleWidget)
     {
         ConsoleWidget->VAddLog(fmt, args);
@@ -56,6 +59,7 @@ void UGlobalConsole::LogV(const char* fmt, va_list args)
         OutputDebugStringA(tmp);
         OutputDebugStringA("\n");
     }
+#endif
 }
 
 // Global C functions for compatibility
