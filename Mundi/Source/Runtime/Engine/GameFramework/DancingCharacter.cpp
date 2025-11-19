d3b481ccd0fcaf5c57fbaacd28b26e783cb4bb57
Sunha-i <icecream20@khu.ac.kr>
2025-11-19 23:16:23 +0900

    feat: add dancing character

---
+++ Mundi/Source/Runtime/Engine/GameFramework/DancingCharacter.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "DancingCharacter.h"
+#include "SkeletalMeshComponent.h"
+
+ADancingCharacter::ADancingCharacter()
+{
+	if (MeshComponent)
+	{
+		MeshComponent->SetSkeletalMesh(GDataDir + "/CactusPA.fbx");
+	}
+}
+
+ADancingCharacter::~ADancingCharacter()
+{
+}
